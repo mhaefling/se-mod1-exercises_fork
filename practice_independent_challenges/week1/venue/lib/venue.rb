dbf84065eaf5fc6db6dfa1a2a9a3646da87a80c2
Matt Haefling <m.haefling@protonmail.com>
2024-10-16 08:16:34 -0600

    Completed week 1 practice lesson

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,21 @@
 class Venue
+    attr_reader :name,
+                :capacity,
+                :patrons
+    
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def add_patron(patron_name)
+        patrons.append(patron_name)
+    end
+
+    def yell_at_patrons
+        patrons_yell = patrons.map do |patron|
+            patron.upcase
+        end
+    end
 end
